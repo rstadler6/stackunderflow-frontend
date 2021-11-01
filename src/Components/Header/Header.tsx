ed2879ef3ccdfd590f0bc769291722c7c688d5df
Raphael Stadler <raphael.stadler@cmiag.ch>
2021-11-01 09:00:50 +0100

    Header fixed

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,3 +1,18 @@
+import React, {FormEvent, useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
+
+export default function Header(props: { setJwt: (content: string) => void }) {
+    return (
+        <div>
             <Link to="/"><button>Entries</button></Link>
             <Link to="/categories"><button>Categories</button></Link>
             <Link to="/users"><button>Users</button></Link>
+            <button onClick={logout}>Logout</button>
+        </div>
+    )
+
+    function logout() {
+        localStorage.clear();
+        props.setJwt("");
+    }
+}
